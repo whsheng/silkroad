43469e563db390d069710c469ad29958ceab27f9
whsheng <1102@mapgoo.net>
2025-01-26 17:51:28 +0800

    hide github info

--- components/site-header/index.tsx
+++ components/site-header/index.tsx
@@ -43,12 +43,16 @@ export function SiteHeader() {
             >
               搜索网站...
             </Button>
+
+            {/* 
             <Link href={siteConfig.links.github} target="_blank" rel="noreferrer">
               <Button className="rounded-full" variant="ghost" size="icon">
                 <Icons.gitHub className="h-5 w-5" />
                 <span className="sr-only">GitHub</span>
               </Button>
-            </Link>
+            </Link> 
+            */}
+
             <Link href={siteConfig.links.twitter} target="_blank" rel="noreferrer">
               <Button className="rounded-full" variant="ghost" size="icon">
                 <Icons.twitter className="h-5 w-5 fill-current" />
